f4501b33ad7e1cf9247061a8adc303b76b882565
Cybertwip <greentwip@cyber-navy.eu>
2025-01-29 00:49:03 +0100

    Reimplement/create new headers for incompatible license code

--- libc/stdlib/vsprintf.c
+++ libc/stdlib/vsprintf.c
@@ -1,231 +1,213 @@
-/* vsprintf.c -- Lars Wirzenius & Linus Torvalds. */
-/*
- * Wirzenius wrote this portably, Torvalds fucked it up :-)
- */
-
 #include <stdarg.h>
-#include "sys/system.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-/* we use this so that we can do without the ctype library */
-#define is_digit(c)	((c) >= '0' && (c) <= '9')
+// Check if a character is a digit (0-9)
+#define is_digit(c) ((c) >= '0' && (c) <= '9')
 
-static int skip_atoi(const char **s)
-{
-	int i=0;
-
-	while (is_digit(**s))
-		i = i*10 + *((*s)++) - '0';
-	return i;
+// Skip leading digits in a string and convert them to an integer
+static int skip_atoi(const char **s) {
+    int value = 0;
+    while (is_digit(**s)) {
+        value = value * 10 + (**s - '0');
+        (*s)++;
+    }
+    return value;
 }
 
-#define ZEROPAD	1		/* pad with zero */
-#define SIGN	  2   /* unsigned/signed long */
-#define PLUS    4	  /* show plus */
-#define SPACE	  8   /* space if plus */
-#define LEFT	  16  /* left justified */
-#define SPECIAL	32  /* 0x */
-#define SMALL	  64  /* use 'abcdef' instead of 'ABCDEF' */
+// Flags for formatting
+#define FLAG_ZEROPAD   (1 << 0)  // Pad with zeros
+#define FLAG_LEFT      (1 << 1)  // Left-justify
+#define FLAG_PLUS      (1 << 2)  // Show plus sign
+#define FLAG_SPACE     (1 << 3)  // Show space for positive numbers
+#define FLAG_SPECIAL   (1 << 4)  // Add '0x' for hex or '0' for octal
+#define FLAG_SMALL     (1 << 5)  // Use lowercase for hex digits
 
-#define do_div(n,base) ({ \
-int __res; \
-__asm__("divl %4":"=a" (n),"=d" (__res):"0" (n),"1" (0),"r" (base)); \
-__res; })
+// Convert a number to a string representation
+static char *number_to_string(char *str, int num, int base, int width, int precision, int flags) {
+    static const char *digits_upper = "0123456789ABCDEF";
+    static const char *digits_lower = "0123456789abcdef";
+    const char *digits = (flags & FLAG_SMALL) ? digits_lower : digits_upper;
+    char buffer[32];  // Temporary buffer for storing digits
+    int index = 0;
+    bool is_negative = false;
 
-static char * number(char * str, int num, int base, int size, int precision
-	,int type)
-{
-	char c,sign,tmp[36];
-	const char *digits="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
+    // Handle negative numbers
+    if (flags & FLAG_PLUS && num < 0) {
+        is_negative = true;
+        num = -num;
+    }
 
-	if (type&SMALL) digits="0123456789abcdefghijklmnopqrstuvwxyz";
-	if (type&LEFT) type &= ~ZEROPAD;
-	if (base<2 || base>36)
-		return 0;
-	c = (type & ZEROPAD) ? '0' : ' ' ;
-	if (type&SIGN && num<0) {
-		sign='-';
-		num = -num;
-	} else
-		sign=(type&PLUS) ? '+' : ((type&SPACE) ? ' ' : 0);
-	if (sign) size--;
-	if (type&SPECIAL)
-		if (base==16) size -= 2;
-		else if (base==8) size--;
-	i=0;
-	if (num==0)
-		tmp[i++]='0';
-	else while (num!=0)
-		tmp[i++]=digits[do_div(num,base)];
-	if (i>precision) precision=i;
-	size -= precision;
-	if (!(type&(ZEROPAD+LEFT)))
-		while(size-->0)
-			*str++ = ' ';
-	if (sign)
-		*str++ = sign;
-	if (type&SPECIAL)
-		if (base==8)
-			*str++ = '0';
-		else if (base==16) {
-			*str++ = '0';
-			*str++ = digits[33];
-		}
-	if (!(type&LEFT))
-		while(size-->0)
-			*str++ = c;
-	while(i<precision--)
-		*str++ = '0';
-	while(i-->0)
-		*str++ = tmp[i];
-	while(size-->0)
-		*str++ = ' ';
-	return str;
+    // Convert the number to a string in reverse order
+    do {
+        buffer[index++] = digits[num % base];
+        num /= base;
+    } while (num > 0);
+
+    // Add padding for precision
+    while (index < precision) {
+        buffer[index++] = '0';
+    }
+
+    // Add sign or space
+    if (is_negative) {
+        buffer[index++] = '-';
+    } else if (flags & FLAG_PLUS) {
+        buffer[index++] = '+';
+    } else if (flags & FLAG_SPACE) {
+        buffer[index++] = ' ';
+    }
+
+    // Add special prefixes for hex or octal
+    if (flags & FLAG_SPECIAL) {
+        if (base == 16) {
+            buffer[index++] = (flags & FLAG_SMALL) ? 'x' : 'X';
+            buffer[index++] = '0';
+        } else if (base == 8) {
+            buffer[index++] = '0';
+        }
+    }
+
+    // Reverse the buffer to get the correct order
+    for (int i = 0; i < index / 2; i++) {
+        char temp = buffer[i];
+        buffer[i] = buffer[index - i - 1];
+        buffer[index - i - 1] = temp;
+    }
+
+    // Add padding for width
+    int padding = width - index;
+    if (padding > 0 && !(flags & FLAG_LEFT)) {
+        char pad_char = (flags & FLAG_ZEROPAD) ? '0' : ' ';
+        while (padding-- > 0) {
+            *str++ = pad_char;
+        }
+    }
+
+    // Copy the buffer to the output string
+    for (int i = 0; i < index; i++) {
+        *str++ = buffer[i];
+    }
+
+    // Add padding for left-justified numbers
+    if (padding > 0 && (flags & FLAG_LEFT)) {
+        while (padding-- > 0) {
+            *str++ = ' ';
+        }
+    }
+
+    return str;
 }
 
-int vsprintf(char *buf, const char *fmt, va_list args)
-{
-	int len;
-	int i;
-	char * str;
-	char *s;
-	int *ip;
+// Custom vsprintf implementation
+int vsprintf(char *buf, const char *fmt, va_list args) {
+    char *str = buf;
 
-	int flags;		/* flags to number() */
+    while (*fmt) {
+        if (*fmt != '%') {
+            *str++ = *fmt++;
+            continue;
+        }
 
-	int field_width;	/* width of output field */
-	int precision;		/* min. # of digits for integers; max
-				   number of chars for from string */
-	int qualifier;		/* 'h', 'l', or 'L' for integer fields */
+        // Parse flags
+        int flags = 0;
+        while (true) {
+            fmt++;
+            switch (*fmt) {
+                case '-': flags |= FLAG_LEFT; break;
+                case '+': flags |= FLAG_PLUS; break;
+                case ' ': flags |= FLAG_SPACE; break;
+                case '#': flags |= FLAG_SPECIAL; break;
+                case '0': flags |= FLAG_ZEROPAD; break;
+                default: goto done_flags;
+            }
+        }
+        done_flags:
 
-	for (str=buf ; *fmt ; ++fmt) {
-		if (*fmt != '%') {
-			*str++ = *fmt;
-			continue;
-		}
-			
-		/* process flags */
-		flags = 0;
-		repeat:
-			++fmt;		/* this also skips first '%' */
-			switch (*fmt) {
-				case '-': flags |= LEFT; goto repeat;
-				case '+': flags |= PLUS; goto repeat;
-				case ' ': flags |= SPACE; goto repeat;
-				case '#': flags |= SPECIAL; goto repeat;
-				case '0': flags |= ZEROPAD; goto repeat;
-				}
-		
-		/* get field width */
-		field_width = -1;
-		if (is_digit(*fmt))
-			field_width = skip_atoi(&fmt);
-		else if (*fmt == '*') {
-			/* it's the next argument */
-			field_width = va_arg(args, int);
-			if (field_width < 0) {
-				field_width = -field_width;
-				flags |= LEFT;
-			}
-		}
+        // Parse field width
+        int width = -1;
+        if (is_digit(*fmt)) {
+            width = skip_atoi(&fmt);
+        } else if (*fmt == '*') {
+            width = va_arg(args, int);
+            fmt++;
+        }
 
-		/* get the precision */
-		precision = -1;
-		if (*fmt == '.') {
-			++fmt;	
-			if (is_digit(*fmt))
-				precision = skip_atoi(&fmt);
-			else if (*fmt == '*') {
-				/* it's the next argument */
-				precision = va_arg(args, int);
-			}
-			if (precision < 0)
-				precision = 0;
-		}
+        // Parse precision
+        int precision = -1;
+        if (*fmt == '.') {
+            fmt++;
+            if (is_digit(*fmt)) {
+                precision = skip_atoi(&fmt);
+            } else if (*fmt == '*') {
+                precision = va_arg(args, int);
+                fmt++;
+            }
+        }
 
-		/* get the conversion qualifier */
-		qualifier = -1;
-		if (*fmt == 'h' || *fmt == 'l' || *fmt == 'L') {
-			qualifier = *fmt;
-			++fmt;
-		}
+        // Parse length modifier (ignored in this implementation)
+        if (*fmt == 'h' || *fmt == 'l' || *fmt == 'L') {
+            fmt++;
+        }
 
-		switch (*fmt) {
-		case 'c':
-			if (!(flags & LEFT))
-				while (--field_width > 0)
-					*str++ = ' ';
-			*str++ = (unsigned char) va_arg(args, int);
-			while (--field_width > 0)
-				*str++ = ' ';
-			break;
+        // Handle format specifiers
+        switch (*fmt) {
+            case 'c': {
+                char c = (char) va_arg(args, int);
+                *str++ = c;
+                break;
+            }
+            case 's': {
+                const char *s = va_arg(args, const char *);
+                while (*s) {
+                    *str++ = *s++;
+                }
+                break;
+            }
+            case 'd':
+            case 'i': {
+                int num = va_arg(args, int);
+                str = number_to_string(str, num, 10, width, precision, flags | FLAG_PLUS);
+                break;
+            }
+            case 'u': {
+                unsigned int num = va_arg(args, unsigned int);
+                str = number_to_string(str, num, 10, width, precision, flags);
+                break;
+            }
+            case 'o': {
+                unsigned int num = va_arg(args, unsigned int);
+                str = number_to_string(str, num, 8, width, precision, flags);
+                break;
+            }
+            case 'x':
+            case 'X': {
+                unsigned int num = va_arg(args, unsigned int);
+                if (*fmt == 'x') {
+                    flags |= FLAG_SMALL;
+                }
+                str = number_to_string(str, num, 16, width, precision, flags);
+                break;
+            }
+            case 'p': {
+                void *ptr = va_arg(args, void *);
+                str = number_to_string(str, (uintptr_t) ptr, 16, width, precision, flags | FLAG_SPECIAL);
+                break;
+            }
+            case '%': {
+                *str++ = '%';
+                break;
+            }
+            default: {
+                *str++ = '%';
+                *str++ = *fmt;
+                break;
+            }
+        }
+        fmt++;
+    }
 
-		case 's':
-			s = va_arg(args, char *);
-			len = strlen(s);
-			if (precision < 0)
-				precision = len;
-			else if (len > precision)
-				len = precision;
-
-			if (!(flags & LEFT))
-				while (len < field_width--)
-					*str++ = ' ';
-			for (i = 0; i < len; ++i)
-				*str++ = *s++;
-			while (len < field_width--)
-				*str++ = ' ';
-			break;
-
-		case 'o':
-			str = number(str, va_arg(args, unsigned long), 8,
-				field_width, precision, flags);
-			break;
-
-		case 'p':
-			if (field_width == -1) {
-				field_width = 8;
-				flags |= ZEROPAD;
-			}
-			str = number(str,
-				(unsigned long) va_arg(args, void *), 16,
-				field_width, precision, flags);
-			break;
-
-		case 'x':
-			flags |= SMALL;
-		case 'X':
-			str = number(str, va_arg(args, unsigned long), 16,
-				field_width, precision, flags);
-			break;
-
-		case 'd':
-		case 'i':
-			flags |= SIGN;
-		case 'u':
-			str = number(str, va_arg(args, unsigned long), 10,
-				field_width, precision, flags);
-			break;
-		case 'b':
-			str = number(str, va_arg(args, unsigned long), 2,
-				field_width, precision, flags);
-			break;
-
-		case 'n':
-			ip = va_arg(args, int *);
-			*ip = (str - buf);
-			break;
-
-		default:
-			if (*fmt != '%')
-				*str++ = '%';
-			if (*fmt)
-				*str++ = *fmt;
-			else
-				--fmt;
-			break;
-		}
-	}
-	*str = '\0';
-	return str-buf;
+    *str = '\0';  // Null-terminate the string
+    return str - buf;  // Return the number of characters written
 }
